618f307fa4ac6e39638259b1eca23b3c83202751
Hoya <kisesesky19@gmail.com>
2025-09-29 14:06:32 +0900

    feat: types정리

---
+++ src/lib/brandedTypes.ts
@@ -0,0 +1,7 @@
+
+// Branded types for strong type safety
+export type ServerId = number & { readonly brand: 'ServerId' };
+export type ChannelId = number & { readonly brand: 'ChannelId' };
+export type UserId = number & { readonly brand: 'UserId' };
+export type MessageId = number & { readonly brand: 'MessageId' };
+export type CategoryId = number & { readonly brand: 'CategoryId' };
