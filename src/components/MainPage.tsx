cd86f146bc31ed21e070fa73f4c591004fa39df2
Hoya <kisesesky19@gmail.com>
2025-10-02 02:39:48 +0900

    feat: dark,light Mode적용

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -26,7 +26,7 @@ export function MainPage({ onLogout }: MainPageProps) {
     <div className='flex flex-row flex-1 h-full'>
       {viewMode === 'server' ? <ChannelList onLogout={onLogout} /> : <FriendsList />}
       <div className='flex-1 flex flex-col bg-white dark:bg-gray-800 text-black dark:text-white min-h-0 h-full'>
-        <div className={`flex-1 flex bg-gray-700 relative h-full`}>
+        <div className={`flex-1 flex bg-gray-200 dark:bg-gray-700 relative h-full`}>
           <div className={`h-full ${isAnyPanelOpen ? 'w-[60%]' : 'flex-1 w-full'}`}>
             <ChatArea />
           </div>
